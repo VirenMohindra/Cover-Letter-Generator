8b27c40484a8e94b05e5f3ab4073212a8e5cc8ed
Viren Mohindra <viren.mohindra@gmail.com>
2016-11-18 20:56:58 -0500

    rm information
    sanitization is important on the web

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Cover-Letter-Generator
+# Cover Letter Generator
 Bash script which takes in variables and outputs a pdf with personalized signature. Highly customizable.
+
+# Installation and Run Time Instructions
+
+0. `cd Desktop/`
+1. `wget https://raw.githubusercontent.com/VirenMohindra/Cover-Letter-Generator/master/cover-generator.sh`
+2. `$ chmod u+x cover-generator.sh`
+3. `$ ./cover-generator.sh`
+
+# To Do List
+1. More functional and fragmented *(how verbose)*
+2. Add signature
+3. Creation of PDF > output to current dir
+4. Clean up installation process and make it simpler than three commands
+
+# Inspiration
+1. Made this gist (Gist)[https://gist.github.com/VirenMohindra/4c303343d055a1e79a2b17ad858db86f]
--- cover-generator.sh
+++ cover-generator.sh
@@ -7,32 +7,32 @@ echo "Automate Cover Letters for Companies"
 echo "__________________________________________________________________________"
 
 # Personal Details
-read -p "Full Name [defaults to Viren Mohindra]: " name
-name=${name:-Viren Mohindra}
+read -p "Full Name [defaults to 1]: " name
+name=${name:-1}
 
-read -p "Class Standing [defaults to Sophomore]: " year
-year=${year:-"Sophomore"}
+read -p "Class Standing [defaults to 1]: " year
+year=${year:-"1"}
 
-read -p "Major [defaults to Film & TV and Computer Science]: " major
-major=${major:-"Film & TV and Computer Science"}
+read -p "Major [defaults to 1]: " major
+major=${major:-"1"}
 
-read -p "College/ University [defaults to New York University]: " college
-college=${college:-"New York University"}
+read -p "College/ University [defaults to 1]: " college
+college=${college:-"1"}
 
-read -p "Address [defaults to 80 Lafayette Street, 0908A, New York, US - 10013]: " address
-address=${address:-"80 Lafayette Street, 0908A, New York, US - 10013"}
+read -p "Address [defaults to 1]: " address
+address=${address:-"1"}
 
-read -p "Website [defaults to http://virenmohindra.me/]: " web
-web=${web:-"http://virenmohindra.me/"}
+read -p "Website [defaults to 1]: " web
+web=${web:-"1"}
 
-read -p "Phone Number [defaults to +1 (240) 848-0818]: " phone
-phone=${phone:-"+1 (240) 848-0818"}
+read -p "Phone Number [defaults to 1]: " phone
+phone=${phone:-"1"}
 
-read -p "Email Address [defaults to viren.mohindra@nyu.edu]: " email
-email=${email:-"viren.mohindra@nyu.edu"}
+read -p "Email Address [1]: " email
+email=${email:-"1"}
 
 read -p "List of countries you've lived in [defaults to Hong Kong, Singapore, and India]: " country
-country=${country:-"Hong Kong, Singapore, and India"}
+country=${country:-"1"}
 
 
 # Company
@@ -50,28 +50,18 @@ industry=${industry:-"Software"}
 now=$(date +"%m-%d-%Y")
 
 # essay
-BS=
-"
+BS="
 $name
 $address
 $web
 $phone
 $now
-
 Hello $poc,
-
 I hope your day is going well. I am applying as a $job at $companyName. As the Co-Founder of a brand development agency, Nuked Studios (http://nukewebbed.com/), I have always needed to interact extensively with clients, while researching and identifying competition. After having lived and studied in $country, I have learnt how to communicate well and assist customers, providing a good product with great service.
-
 On the technical side, I am currently a $year studying $major at $college. In my spare time, I pursue website development and am also the Chief Technology Officer for The Young Post (http://theyoungpost.com/). Some of my responsibilities include collaborating with a number of undergraduate ivy league undergraduates and handling a staff of 130 capable writers.
-
 I manage and follow Google Analytics for all the aforementioned sites; having recently implemented heat maps and visitor recordings to effectively capture target demographics. Using this data, I ensure conversions to the numerous sites I own, increasing subscriber count.
-
 I would appreciate an opportunity at $companyName to further my knowledge in the $industry business. Please let me know if there's anything else you need from me. I look forward to hearing from you. My attached resume details additional work experience. Thank you for your time and consideration in reviewing my application.
-
 Best Regards,
-
-
-
 
 $name
 Email: $email or
@@ -79,7 +69,7 @@ Cell: $phone
 "
 
 # creating cover letter
-cat <<EOT >> "$Cover_Letter.txt"
+cat <<EOT >> "$name_Cover_Letter.txt"
 $BS
 EOT
 
